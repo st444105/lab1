5aa25b71892ef419cc4fa022b7a222f7a6464de3
st444105 <ucheba55348@mail.ru>
2026-09-20 17:18:13 +0700

    Данные одного противника

---
+++ lab1/Models/CEnemyTemplate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace lab1.Models
+{
+    public class CEnemyTemplate
+    {
+        [JsonInclude]
+        public string Name { get; private set; }
+
+        [JsonInclude]
+        public string IconName { get; private set; }
+
+        [JsonInclude]
+        public int BaseLife { get; private set; }
+
+        [JsonInclude]
+        public double LifeModifier { get; private set; }
+
+        [JsonInclude]
+        public int BaseGold { get; private set; }
+
+        [JsonInclude]
+        public double GoldModifier { get; private set; }
+
+        [JsonInclude]
+        public double SpawnChance { get; private set; }
+
+        public CEnemyTemplate(
+            string name,
+            string iconName,
+            int baseLife,
+            double lifeModifier,
+            int baseGold,
+            double goldModifier,
+            double spawnChance)
+        {
+            Name = name;
+            IconName = iconName;
+            BaseLife = baseLife;
+            LifeModifier = lifeModifier;
+            BaseGold = baseGold;
+            GoldModifier = goldModifier;
+            SpawnChance = spawnChance;
+        }
+    }
+}
