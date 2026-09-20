18585e5d61fdfd27df8c7c84c6cec345d554ebd4
st444105 <ucheba55348@mail.ru>
2026-09-20 17:22:40 +0700

    Данные иконки противника

---
+++ lab1/Models/EnemyIcon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1.Models
+{
+    public class EnemyIcon
+    {
+        public string Name { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public EnemyIcon(string name, string imagePath)
+        {
+            Name = name;
+            ImagePath = imagePath;
+        }
+    }
+}
